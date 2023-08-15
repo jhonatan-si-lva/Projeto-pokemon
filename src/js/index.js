d0303874ee7b905aeba736ab187984d15b471d08
jhonatan-si-lva <133882876+jhonatan-si-lva@users.noreply.github.com>
2023-08-15 19:44:51 -0300

    Add files via upload
    Projeto cartão pokémon, utilizado para o desenvolvimento HTML,CSS e JAVA SCRIPT.

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const botaoAlterarTema = document.getElementById ("botão-alterar-tema"); 
+
+const body = document.querySelector ("body");
+
+const imagemBotaoTrocadeTema = document.querySelector (".imagem-botao");
+
+botaoAlterarTema.addEventListener ("click", () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains ("modo-escuro");
+
+    body.classList.toggle ("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+     imagemBotaoTrocadeTema.setAttribute ("src", "./src/imagens/sun.png");
+
+    } else {
+
+      imagemBotaoTrocadeTema.setAttribute ("src", "./src/imagens/moon.png");
+    }
+
+});
